77d95235e384020dbb6557eea275762b66b9e436
luc-ted <luc@tedorthopedics.com>
2023-01-28 14:57:57 +0100

    Coment and explain data_exploration notebook and associated files.

--- lib/database/dataset.py
+++ lib/database/dataset.py
@@ -40,20 +40,21 @@ def build_dataset_base(db_path, data_paths, label=0,
     return dataset
 
 
+# build dataset from data_path dataframe
 def build_dataset(db_path, data_paths,
-                  db=[lung_name], ds=[train_name],
+                  db=[lung_name], ds=[train_name], data_type=[images_name],
                   **kwargs):
 
     idx = pd.IndexSlice
 
-    paths = data_paths.loc[idx[db, ds, normal_name, [images_name]]]
+    paths = data_paths.loc[idx[db, ds, normal_name, data_type]]
     dataset = build_dataset_base(db_path, paths, label=[1, 0, 0], **kwargs)
 
-    paths = data_paths.loc[idx[db, ds, covid_name, [images_name]]]
+    paths = data_paths.loc[idx[db, ds, covid_name, data_type]]
     ds_tmp = build_dataset_base(db_path, paths, label=[0, 1, 0], **kwargs)
     dataset = dataset.concatenate(ds_tmp)
 
-    paths = data_paths.loc[idx[db, ds, no_covid_name, [images_name]]]
+    paths = data_paths.loc[idx[db, ds, no_covid_name, data_type]]
     ds_tmp = build_dataset_base(db_path, paths, label=[0, 0, 1], **kwargs)
     dataset = dataset.concatenate(ds_tmp)
 
--- lib/database/path_origin_data.py
+++ lib/database/path_origin_data.py
@@ -1,78 +1,92 @@
 import os
 import pandas as pd
 
-# data names
+# database names
 lung_name = 'lung'
 infection_name = 'infection'
 
+# dataset names
 train_name = 'train'
 test_name = 'test'
 valid_name = 'valid'
 
+# label / desease names
 no_covid_name = 'no_covid'
 covid_name = 'covid19'
 normal_name = 'normal'
 
+# data types names
 images_name = 'images'
 masks_name = 'masks'
 lung_mask_name = lung_name + '_' + masks_name
 infection_mask_name = infection_name + '_' + masks_name
 
-# data directory names
+# database directory names
 lung_dirname = 'Lung Segmentation Data'
 infection_dirname = 'Infection Segmentation Data'
 
+# data set names
 train_dirname = 'Train'
 test_dirname = 'Test'
 valid_dirname = 'Val'
 
+# label / desease names
 no_covid_dirname = 'Non-COVID'
 covid_dirname = 'COVID-19'
 normal_dirname = 'Normal'
 
+# data type names
 images_dirname = 'images'
 lung_masks_dirname = 'lung masks'
 infection_masks_dirname = 'infection masks'
 
-db_names = pd.DataFrame(data={
-    'db_name': [lung_name, infection_name],
-    'db_dirname': [lung_dirname, infection_dirname]
-})
-ds_names = pd.DataFrame(data={
-    'ds_name': [train_name, test_name, valid_name],
-    'ds_dirname': [train_dirname, test_dirname, valid_dirname]
-})
-desease_names = pd.DataFrame(data={
-    'desease_name': [no_covid_name, covid_name, normal_name],
-    'desease_dirname': [no_covid_dirname, covid_dirname, normal_dirname]
-})
-data_type_names = pd.DataFrame(data={
-    'data_type_name': [images_name, lung_mask_name, infection_mask_name],
-    'data_type_dirname': [images_dirname, lung_masks_dirname, infection_masks_dirname]
-})
 
-# Dataset
-
-
+# build all the path available in databases
 def build_data_paths():
+    """
+    output: data_path dataFrame
+        * with index= ['db_name', 'ds_name', 'desease_name', 'data_type_name']
+        * path: folders hierarchical string path 
+    """
+
+    # dataframe structure to link code names and real names
+    db_names = pd.DataFrame(data={
+        'db_name': [lung_name, infection_name],
+        'db_dirname': [lung_dirname, infection_dirname]
+    })
+    ds_names = pd.DataFrame(data={
+        'ds_name': [train_name, test_name, valid_name],
+        'ds_dirname': [train_dirname, test_dirname, valid_dirname]
+    })
+    desease_names = pd.DataFrame(data={
+        'desease_name': [no_covid_name, covid_name, normal_name],
+        'desease_dirname': [no_covid_dirname, covid_dirname, normal_dirname]
+    })
+    data_type_names = pd.DataFrame(data={
+        'data_type_name': [images_name, lung_mask_name, infection_mask_name],
+        'data_type_dirname': [images_dirname, lung_masks_dirname, infection_masks_dirname]
+    })
+
+    # add keys for merging dataframes
     db_names['keys'] = 1
     ds_names['keys'] = 1
     desease_names['keys'] = 1
     data_type_names['keys'] = 1
 
-    # db_names
+    # merging
     data_paths = pd.merge(db_names, ds_names, on='keys')
     data_paths = pd.merge(data_paths, desease_names, on='keys')
     data_paths = pd.merge(data_paths, data_type_names, on='keys')
     data_paths = data_paths.drop(columns=['keys'])
-    # data_paths
+
+    # build data_paths
     data_paths = data_paths.set_index(
         ['db_name', 'ds_name', 'desease_name', 'data_type_name'])
     data_paths['path'] = data_paths.apply(
         lambda x: os.path.join(*(x.to_list())), axis=1)
     data_paths = data_paths.iloc[:, -1:]
 
-    # data_paths
+    # drop unpresent infection mask under lung data base
     idx = pd.IndexSlice
     index = data_paths.loc[idx[[lung_name], :, :, infection_mask_name]].index
     data_paths = data_paths.drop(index)
